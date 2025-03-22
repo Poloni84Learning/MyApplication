599fe743fc3a78949906d7084a8c29dcc0425962
PhamAnhTu2409 <long0317106@gmail.com>
2025-03-22 20:57:41 +0700

    New

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                AppNavigation() // Sử dụng AppNavigation
+                AppNavigation()
             }
         }
     }
