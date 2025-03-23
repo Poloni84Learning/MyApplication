7c769bb37495944c739c5cf4f1270a901df5113d
PhamAnhTu2409 <long0317106@gmail.com>
2025-03-23 20:58:38 +0700

    Update systems

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,17 @@ android {
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
 
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.moshi:moshi:1.14.0")
+    implementation ("com.squareup.moshi:moshi-kotlin:1.14.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.5.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    implementation ("com.google.code.gson:gson:2.10.1")
+
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
